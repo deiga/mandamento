77720b604df252ee5d3f8f1088c771231e063ea3
Timo Sand <timo.sand@iki.fi>
2024-06-02 20:22:59 +0300

    Replace local docker postgres with hosted from Neon
    Signed-off-by: Timo Sand <timo.sand@iki.fi>

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { neon } from '@neondatabase/serverless';
+
+console.log('Hello via Bun!');
+
+// Bun automatically loads the DATABASE_URL from .env.local
+// Refer to: https://bun.sh/docs/runtime/env for more information
+const sql = neon(process.env.DATABASE_URL!);
+
+const rows = await sql`SELECT version()`;
+
+console.log(rows[0].version);
